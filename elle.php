92b7a9fa2b2d288bcddb85ba7d06e422c41c2d5d
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2017-11-13 10:34:43 +0100

    påbörjan uppgift 4

--- elle.php
+++ elle.php
@@ -18,24 +18,54 @@ $words = [
 
 ];
 
-
-
-
+//read the contents of a file into a string
 $content = file_get_contents($fileName);
 
-// * Ta bort alla html-taggar (finns inbyggd funktion för det)
+//strängen tas bort från html, php taggar
 $content = strip_tags($content);
+
+//The explode() function breaks a string into an array.
+$content = explode(" ", $content );
+
+
+foreach ($words as $word => $amount) {
+    foreach ($content as $item) {
+        if (strpos($item, $word) !== false) {
+            $words[$word] = $words[$word] + 1;
+        }
+    }
+ }
+var_dump($words); // skriver ut array med ord och antalet ord
+
+
+ function count_words($text, array $words) {
+
+ }
+
+//Funktionen ska spara ned i en fil
+//Analysera förekomsten av utvalda ord i innehållet
+
+/*function count_words($content, array $words ) {
+
+
+
+    foreach ($words as $word => $count) {
+        $words[$word] = substr_count($content, $word);
+    }
+    var_dump($words);
+}*/
+
+
+
+// * Ta bort alla html-taggar (finns inbyggd funktion för det)
+
+
+
 //echo $content;
-
-
 // * Dela upp textsträngen (finns inbyggt funktion)
-
 
 //* Räkna orden (en loop kanske?)
 
 
-foreach ($words as $word => $count) {
-    $words[$word] = substr_count($content, $word);
-}
+//
 
-var_dump($words);
